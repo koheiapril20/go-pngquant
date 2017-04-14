e97c3fd6c665d31c15b12baa5869fea6ff23907f
Kohei Morino <morino_kouhei@cyberagent.co.jp>
2017-04-14 16:44:27 +0900

    Fix std flag

--- pngquant.go
+++ pngquant.go
@@ -5,7 +5,7 @@ package pngquant
 #cgo CFLAGS: -I./internal/pngquant/lib
 #cgo CFLAGS: -I./internal/pngquant
 #cgo CFLAGS: -I./internal
-#cgo CFLAGS: -std=c11
+#cgo CFLAGS: -std=c99
 #include "internal/pngquant/pngquant.h"
 #include <stdio.h>
 #include <stdlib.h>
